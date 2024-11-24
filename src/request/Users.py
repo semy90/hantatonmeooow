11e2304b56abcbe7b0376fcf8b088c235b1f0991
Negrov <galimovgal@mail.ru>
2024-11-24 21:34:06 +0500

    add req add requests der add class Auth  ◹ reg  ◹ login

---
+++ src/request/Users.py
@@ -0,0 +1,35 @@
+import aiohttp
+import asyncio
+
+
+class Auth:
+    @staticmethod
+    async def register(login, password, email, lastname, firstname, middlename, phone, birthday, releid, typeusr):
+        async with aiohttp.ClientSession() as session:
+            async with session.post('https://test.vcc.uriit.ru/auth/register', json={
+                "login": login,
+                "password": password,
+                "email": email,
+                "lastName": lastname,
+                "firstName": firstname,
+                "middleName": middlename,
+                "phone": phone,
+                "birthday": birthday,
+                "roleId": releid,
+                "type": typeusr
+            }) as res:
+                if res.status == 201:
+                    return 1
+                return res.status
+
+    @staticmethod
+    async def login(login, passwd):
+        async with aiohttp.ClientSession() as session:
+            async with session.post('https://test.vcc.uriit.ru/auth/login', json={
+                "login": login,
+                "password": passwd,
+                "fingerprint": {}
+            }) as res:
+                if res.status == 200:
+                    return res.json()
+                return res.status
