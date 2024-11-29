e5710ca548a3992fe418ac9ae7479a1ab4731d9d
semyon <semenglinskikh@gmail.com>
2024-11-29 13:26:07 +0500

    mini update

--- src/request/utils.py
+++ src/request/utils.py
@@ -1,16 +1,17 @@
 import jwt as pyjwt
 import aiohttp
 import base64
-import json
+import json as jsonchik
+
 
 async def jwttort(jwt):
-
-    rt = (jwt['token'] if type(jwt) == dict else jwt).split('.')[1]
+    rt = (jwt['token'] if (type(jwt)) == dict else jwt).split('.')[1]
     rt = rt.encode('utf-8')
     rt = base64.b64decode(rt + b'=' * (-len(rt) % 4))
     rt = rt.decode('utf-8')
-    rt = json.loads(rt)
+    rt = jsonchik.loads(rt)
     return rt
+
 
 async def update_jwt(jwt, session):
     rt = (await pyjwt.decode(jwt, 'secret', algorithms=['HS256']))['refresh_token']
@@ -21,10 +22,12 @@ async def update_jwt(jwt, session):
             jwt = await append_rt(await res.json())
     return jwt
 
+
 async def append_rt(jwt):
     rt = await jwttort(jwt)
     jwt['refresh_token'] = rt['refresh_token']
     return jwt
+
 
 async def second_req(jwt, url, session, json):
     jwt = await update_jwt(jwt, session)
